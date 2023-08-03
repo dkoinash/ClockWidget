de232a53e1d1f58ff8e0a0c08070e32d64b43bfd
Denis Koinash <d.koinash@cyclone-nov.ru>
2023-08-03 17:25:11 +0300

    Реализован циферблат, стрелки и ход стрелок

--- include/widget.h
+++ include/widget.h
@@ -1,6 +1,34 @@
 #pragma once
 
+#include <cmath> // std::sin, std::cos, std::abs, std::truncf, std::fmod
+
+#include <QColor>
+#include <QLinef>
+#include <QPaintEvent>
+#include <QPolygon>
+#include <QVector>
 #include <QWidget>
+
+/// Отступ габаритов квадрата виджета от размеров циферблата
+#define OFFSET 10
+/// Радиус скругления виджета
+#define ROUNDING 10
+/// Размер циферблата
+#define CLOCK_FACE_SIZE 240
+/// Размер рисок циферблата (доля от размера циферблата)
+#define LABEL_SIZE 5
+/// Толщина основных рисок
+#define LABEL_MAIN_WIDTH 1
+/// Толщина пятисекундных рисок
+#define LABEL_FIFTH_WIDTH 2
+/// Размер часовой стрелки (доля от размера циферблата)
+#define ARROW_HOUR_SIZE 50
+/// Размер минутной стрелки (доля от размера циферблата)
+#define ARROW_MINUTE_SIZE 80
+/// Размер секундной стрелки (доля от размера циферблата)
+#define ARROW_SECOND_SIZE 95
+/// Отступ дляч рамеки секундной стрелки
+#define ARROW_SECOND_OFFSET 3
 
 class Widget : public QWidget
 {
@@ -19,8 +47,15 @@ public:
    */
   ~Widget();
 
+private:
+  /**
+   * @brief Подготовка циферблата
+   *
+   */
+  void Calculations();
+
 private slots:
-  
+
   /**
    * @brief Шаг времени
    *
@@ -34,4 +69,26 @@ protected:
    * @param[in] event Событие отрисовки виджета
    */
   void paintEvent(QPaintEvent* event) override;
+
+private:
+  QPoint m_center = QPoint((CLOCK_FACE_SIZE + OFFSET) / 2, (CLOCK_FACE_SIZE + OFFSET) / 2);
+  /// Цвет рамки
+  const QColor m_colorFrame = Qt::black;
+  /// Цвет циферблата
+  const QColor m_colorClockFace = Qt::white;
+  /// Цвет рисок циферблата
+  const QColor m_colorLabels = Qt::black;
+  /// Цвет часовой и минутной стрелок
+  const QColor m_colorMainArrows = Qt::black;
+  /// Цвет секундной стрелки
+  const QColor m_colorSecondArrow = Qt::red;
+  /// Риски циферблата секунд
+  QVector<QLineF> m_labels;
+
+  /// Полигон часовой стрелки
+  QPolygon m_polygonArrowHour;
+  /// Полигон минутной стрелки
+  QPolygon m_polygonArrowMinute;
+  /// Полигон секундной стрелки
+  QPolygon m_polygonArrowSecond;
 };
--- src/widget.cpp
+++ src/widget.cpp
@@ -1,14 +1,153 @@
 #include <widget.h>
 
+#include <QPainter>
+#include <QPen>
+#include <QPoint>
+#include <QSize>
+#include <QTime>
+#include <QTimer>
+
+QVector<QLineF>
+makeNetRays(QPoint center, int radius, int offset, int sector_count)
+{
+  QVector<QLineF> result;
+
+  qreal angle = qreal(2 * 3.14159) / qreal(sector_count);
+  for (int i = 0; i < sector_count; ++i) {
+    result.push_back(
+      QLineF(QPointF(radius * std::cos(angle * i), radius * std::sin(angle * i)) + center,
+             QPointF((offset)*std::cos(angle * i), (offset)*std::sin(angle * i)) + center));
+  }
+  return result;
+}
+
 Widget::Widget(QWidget* parent)
   : QWidget(parent)
-{}
+{
+  setWindowFlag(Qt::FramelessWindowHint);
+  setAttribute(Qt::WA_TranslucentBackground);
+  resize(QSize(CLOCK_FACE_SIZE + OFFSET, CLOCK_FACE_SIZE + OFFSET));
+  Calculations();
+  QTimer* timer = new QTimer();
+  connect(timer, SIGNAL(timeout()), this, SLOT(SecondTakt()));
+  timer->start(100); // И запустим таймер
+}
 
 Widget::~Widget() {}
 
 void
-Widget::SecondTakt();
+Widget::SecondTakt()
+{
+  Calculations();
+  update();
+}
+
+void
+Widget::Calculations()
+{
+  int labelSize       = (CLOCK_FACE_SIZE / 2) * LABEL_SIZE / 100;
+  int labelFarRadius  = (CLOCK_FACE_SIZE / 2) - labelSize;
+  int labelNearRadius = labelFarRadius - labelSize;
+  m_labels            = makeNetRays(m_center, labelFarRadius, labelNearRadius, 60);
+#pragma region "Часовая стрелка"
+  m_polygonArrowHour << QPoint(-1, -1) << QPoint(1, -1)
+                     << QPoint(1, -labelNearRadius * ARROW_HOUR_SIZE / 100)
+                     << QPoint(-1, -labelNearRadius * ARROW_HOUR_SIZE / 100);
+#pragma endregion
+#pragma region "Минутная стрелка"
+  m_polygonArrowMinute << QPoint(-1, -1) << QPoint(1, -1)
+                       << QPoint(1, -labelNearRadius * ARROW_MINUTE_SIZE / 100)
+                       << QPoint(-1, -labelNearRadius * ARROW_MINUTE_SIZE / 100);
+#pragma endregion
+#pragma region "Секундная стрелка"
+  m_polygonArrowSecond << QPoint(0, 0) << QPoint(0, -labelNearRadius + ARROW_SECOND_OFFSET)
+                       << QPoint(ARROW_SECOND_OFFSET, -labelNearRadius + ARROW_SECOND_OFFSET)
+                       << QPoint(ARROW_SECOND_OFFSET, -labelFarRadius - ARROW_SECOND_OFFSET)
+                       << QPoint(-ARROW_SECOND_OFFSET, -labelFarRadius - ARROW_SECOND_OFFSET)
+                       << QPoint(-ARROW_SECOND_OFFSET, -labelNearRadius + ARROW_SECOND_OFFSET)
+                       << QPoint(0, -labelNearRadius + ARROW_SECOND_OFFSET);
+#pragma endregion
+}
 
 void
 Widget::paintEvent(QPaintEvent* event)
-{}
+{
+  QPen     pen;
+  QPainter painter(this);
+  painter.setRenderHint(QPainter::Antialiasing);
+  painter.setRenderHint(QPainter::TextAntialiasing);
+#pragma region "Рамка"
+
+  painter.setBrush(m_colorFrame);
+  painter.setPen(Qt::NoPen);
+  painter.drawRoundedRect(
+    QRect(QPoint(0, 0), QSize(CLOCK_FACE_SIZE + OFFSET, CLOCK_FACE_SIZE + OFFSET)),
+    qreal(ROUNDING),
+    qreal(ROUNDING));
+#pragma endregion
+#pragma region "Циферблат"
+
+  painter.setBrush(m_colorClockFace);
+  painter.setPen(Qt::NoPen);
+  painter.drawEllipse(
+    QRect(QPoint(OFFSET / 2, OFFSET / 2), QSize(CLOCK_FACE_SIZE, CLOCK_FACE_SIZE)));
+#pragma region "Риски"
+  pen.setColor(m_colorLabels);
+  pen.setStyle(Qt::SolidLine);
+  for (size_t i = 0; i < 60; ++i) {
+    if (i == 0 || i % 5 == 0)
+      pen.setWidth(LABEL_FIFTH_WIDTH);
+    else
+      pen.setWidth(LABEL_MAIN_WIDTH);
+    painter.setPen(pen);
+    painter.drawLine(m_labels[i]);
+  }
+
+#pragma endregion
+#pragma region "Цифры"
+// TODO Цифры на циферблате и другие подписи? + дата?
+#pragma endregion
+
+#pragma endregion
+  QTime currentTime       = QTime::currentTime();
+  int   currentTimeHour   = currentTime.hour();
+  int   currentTimeMinute = currentTime.minute();
+  int   currentTimeSecond = currentTime.second();
+  int   currentTimeMSec   = currentTime.msec();
+
+#pragma region "Часовая стрелка"
+  painter.save();
+  painter.translate(m_center);
+  painter.rotate(currentTimeHour * 30 + currentTimeMinute / 2);
+  pen.setColor(m_colorMainArrows);
+  pen.setStyle(Qt::SolidLine);
+  painter.setPen(pen);
+  painter.setBrush(m_colorMainArrows);
+  painter.drawPolygon(m_polygonArrowHour);
+  painter.restore();
+#pragma endregion
+#pragma region "Минутная стрелка"
+  painter.save();
+  painter.translate(m_center);
+  painter.rotate(currentTimeMinute * 6 + currentTimeSecond / 10);
+  pen.setColor(m_colorMainArrows);
+  pen.setStyle(Qt::SolidLine);
+  painter.setPen(pen);
+  painter.setBrush(m_colorMainArrows);
+  painter.drawPolygon(m_polygonArrowMinute);
+  painter.restore();
+#pragma endregion
+#pragma region "Секундная стрелка"
+  painter.save();
+  painter.translate(m_center);
+  painter.rotate(currentTimeSecond * 6 + currentTimeMSec * 6 / 1000);
+  pen.setColor(m_colorSecondArrow);
+  pen.setStyle(Qt::SolidLine);
+  painter.setPen(pen);
+  painter.drawPolyline(m_polygonArrowSecond);
+  painter.restore();
+#pragma endregion
+
+  painter.end();
+  QWidget::paintEvent(event);
+}
