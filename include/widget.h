0b1388bcb8c1f1df5f2d9b265c164d92b0b03059
Denis Koinash <d.koinash@cyclone-nov.ru>
2023-08-04 15:10:50 +0300

    Переделаны стрелки + плавность хода стрелок

--- include/widget.h
+++ include/widget.h
@@ -23,20 +23,44 @@
 #define LABEL_FIFTH_WIDTH 4
 /// Размер часовой стрелки (доля от размера циферблата)
 #define ARROW_HOUR_SIZE 50
+/// Размер "хвостика" часовой стрелки (доля от размера циферблата)
+#define ARROW_HOUR_TAIL_SIZE 25
 /// Толщина часовой стрелки
 #define ARROW_HOUR_WIDTH 8
+/// Толщина линии часовой стрелки
+#define ARROW_HOUR_LINE_WIDTH 6
+/// Радиус центрального круга стрелки
+#define ARROW_HOUR_CENTER_CIRCLE 10
 /// Размер минутной стрелки (доля от размера циферблата)
-#define ARROW_MINUTE_SIZE 90
-/// Толщина часовой стрелки
+#define ARROW_MINUTE_SIZE 80
+/// Толщина минутной стрелки
 #define ARROW_MINUTE_WIDTH 6
+/// Размер "хвостика" минутной стрелки (доля от размера циферблата)
+#define ARROW_MINUTE_TAIL_SIZE 20
+/// Толщина минутной стрелки
+#define ARROW_MINUTE_WIDTH 6
+/// Толщина линии минутной стрелки
+#define ARROW_MINUTE_LINE_WIDTH 4
+/// Радиус центрального круга стрелки
+#define ARROW_MINUTE_CENTER_CIRCLE 8
 /// Размер секундной стрелки (доля от размера циферблата)
-#define ARROW_SECOND_SIZE 95
+#define ARROW_SECOND_SIZE 96
 /// Отступ для размеки секундной стрелки
 #define ARROW_SECOND_OFFSET 4
-/// Длина тонкой части стрелки как отступ от центра для стрелкок (доля от размера циферблата)
-#define ARROWS_OFFSET 20
+/// Толщина секундной стрелки
+#define ARROW_SECOND_WIDTH 1
+/// Размер "хвостика" секундной стрелки (доля от размера циферблата)
+#define ARROW_SECOND_TAIL_SIZE 15
+/// Толщина секундной стрелки
+#define ARROW_SECOND_WIDTH 1
+/// Толщина линии секундной стрелки
+#define ARROW_SECOND_LINE_WIDTH 1
+/// Радиус центрального круга стрелки
+#define ARROW_SECOND_CENTER_CIRCLE 6
 /// Радиус в центре циферблата для отступа начала стрелок (доля от размера циферблата)
 #define ARROWs_CENTER_OFFSET 5
+
+// #define ARROWS_OFFSET 5
 
 class Widget : public QWidget
 {
@@ -61,6 +85,27 @@ private:
    *
    */
   void Calculations();
+  /**
+   * @brief Подготовка виджета часовой стрелки
+   *
+   */
+  void Arrow(QPainter& painter,
+             int       centralCircleRadius,
+             int       mainSize,
+             int       tailSize,
+             QColor    color = Qt::black);
+
+  // /**
+  //  * @brief Подготовка виджета минутной стрелки
+  //  *
+  //  */
+  // void ArrowMinure();
+
+  // /**
+  //  * @brief Подготовка виджета секундной стрелки
+  //  *
+  //  */
+  // void ArrowSecond();
 
 private slots:
 
@@ -79,7 +124,7 @@ protected:
   void paintEvent(QPaintEvent* event) override;
 
 private:
-  QPoint m_center = QPoint((CLOCK_FACE_SIZE + OFFSET) / 2, (CLOCK_FACE_SIZE + OFFSET) / 2);
+  QPoint m_center = QPoint(CLOCK_FACE_SIZE / 2 + OFFSET, CLOCK_FACE_SIZE / 2 + OFFSET);
   /// Цвет рамки
   const QColor m_colorFrame = Qt::black;
   /// Цвет циферблата
@@ -95,12 +140,18 @@ private:
   /// Риски циферблата секунд
   QVector<QLineF> m_labels;
 
-  /// Полигон часовой стрелки
-  QPolygon m_polygonArrowHour;
-  /// Полигон минутной стрелки
-  QPolygon m_polygonArrowMinute;
-  // /// Полигон секундной стрелки
-  // QPolygon m_polygonArrowSecond;
-  /// Линия сенкундной стрелки
-  QLine m_lineArrowSecond;
+  // /// Полигон часовой стрелки
+  // QPolygon m_polygonArrowHour;
+  // /// Полигон минутной стрелки
+  // QPolygon m_polygonArrowMinute;
+  // // /// Полигон секундной стрелки
+  // // QPolygon m_polygonArrowSecond;
+  // /// Линия сенкундной стрелки
+  // QLine m_lineArrowSecond;
+  // /// Виджет часовой стрелки
+  // QImage m_imgArrowHour;
+  // /// Виджет минутной стрелки
+  // QImage m_imgArrowMinute;
+  // /// Виджет секундной стрелки
+  // QImage m_imgArrowSecond;
 };
--- src/widget.cpp
+++ src/widget.cpp
@@ -26,11 +26,11 @@ Widget::Widget(QWidget* parent)
 {
   setWindowFlag(Qt::FramelessWindowHint);
   setAttribute(Qt::WA_TranslucentBackground);
-  resize(QSize(CLOCK_FACE_SIZE + OFFSET, CLOCK_FACE_SIZE + OFFSET));
+  resize(QSize(CLOCK_FACE_SIZE + OFFSET * 2, CLOCK_FACE_SIZE + OFFSET * 2));
   Calculations();
   QTimer* timer = new QTimer();
   connect(timer, SIGNAL(timeout()), this, SLOT(SecondTakt()));
-  timer->start(100); // И запустим таймер
+  timer->start(50); // И запустим таймер
 }
 
 Widget::~Widget() {}
@@ -50,23 +50,24 @@ Widget::Calculations()
   int labelNearRadius = labelFarRadius - labelSize;
   m_labels            = makeNetRays(m_center, labelFarRadius, labelNearRadius, 60);
 #pragma region "Часовая стрелка"
-  m_polygonArrowHour << QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100)
-                     << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100)
-                     << QPoint(ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
-                     << QPoint(ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROW_HOUR_SIZE / 100)
-                     << QPoint(-ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROW_HOUR_SIZE / 100)
-                     << QPoint(-ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
-                     << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100);
+  // m_polygonArrowHour << QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100)
+  //                    << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                    << QPoint(ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                    << QPoint(ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROW_HOUR_SIZE / 100)
+  //                    << QPoint(-ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROW_HOUR_SIZE / 100)
+  //                    << QPoint(-ARROW_HOUR_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                    << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100);
 #pragma endregion
 #pragma region "Минутная стрелка"
-  m_polygonArrowMinute << QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100)
-                       << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100)
-                       << QPoint(ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
-                       << QPoint(ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROW_MINUTE_SIZE / 100)
-                       << QPoint(-ARROW_MINUTE_WIDTH / 2,
-                                 -labelNearRadius * ARROW_MINUTE_SIZE / 100)
-                       << QPoint(-ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
-                       << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100);
+  // m_polygonArrowMinute << QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100)
+  //                      << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                      << QPoint(ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                      << QPoint(ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROW_MINUTE_SIZE /
+  //                      100)
+  //                      << QPoint(-ARROW_MINUTE_WIDTH / 2,
+  //                                -labelNearRadius * ARROW_MINUTE_SIZE / 100)
+  //                      << QPoint(-ARROW_MINUTE_WIDTH / 2, -labelNearRadius * ARROWS_OFFSET / 100)
+  //                      << QPoint(0, -labelNearRadius * ARROWS_OFFSET / 100);
 #pragma endregion
 #pragma region "Секундная стрелка"
   // m_polygonArrowSecond << QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100)
@@ -76,9 +77,41 @@ Widget::Calculations()
   //                      << QPoint(-ARROW_SECOND_OFFSET, -labelFarRadius - ARROW_SECOND_OFFSET)
   //                      << QPoint(-ARROW_SECOND_OFFSET, -labelNearRadius + ARROW_SECOND_OFFSET)
   //                      << QPoint(0, -labelNearRadius + ARROW_SECOND_OFFSET);
-  m_lineArrowSecond = QLine(QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100),
-                            QPoint(0, -labelNearRadius * ARROW_SECOND_SIZE / 100));
+  // m_lineArrowSecond = QLine(QPoint(0, -labelNearRadius * ARROWs_CENTER_OFFSET / 100),
+  //                           QPoint(0, -labelNearRadius * ARROW_SECOND_SIZE / 100));
 #pragma endregion
+}
+
+void
+Widget::Arrow(QPainter& painter, int centralCircleRadius, int mainSize, int tailSize, QColor color)
+{
+  // painter.setRenderHint(QPainter::Antialiasing);
+  // painter.setRenderHint(QPainter::TextAntialiasing);
+
+  painter.setPen(Qt::NoPen);
+  painter.setBrush(color);
+  // Центральный круг
+  painter.drawEllipse(QRect(QPoint(0 - centralCircleRadius, 0 - centralCircleRadius),
+                            QPoint(0 + centralCircleRadius, 0 + centralCircleRadius)));
+  QPen pen;
+  pen.setColor(color);
+  pen.setStyle(Qt::SolidLine);
+  pen.setWidth(ARROW_HOUR_LINE_WIDTH);
+  painter.setPen(pen);
+  // Стрелка
+  int arrowSize     = (CLOCK_FACE_SIZE / 2) * mainSize / 100;
+  int arrowTailSize = (CLOCK_FACE_SIZE / 2) * tailSize / 100;
+  painter.drawLine(QPoint(0, 0), QPoint(0, 0 - arrowSize));
+  // Треугольник хвоста
+  painter.setPen(Qt::NoPen);
+  // QPolygon triangle;
+  // triangle << QPoint(0, 0) << QPoint(-arrowTailSize * 1 / 3, arrowTailSize)
+  //          << QPoint(arrowTailSize * 1 / 3, arrowTailSize);
+  // painter.drawPolygon(triangle);
+  painter.drawPie(
+    QRect(QPoint(-arrowTailSize, -arrowTailSize), QPoint(arrowTailSize, arrowTailSize)),
+    -60 * 16,
+    -60 * 16);
 }
 
 void
@@ -93,7 +126,7 @@ Widget::paintEvent(QPaintEvent* event)
   painter.setBrush(m_colorFrame);
   painter.setPen(Qt::NoPen);
   painter.drawRoundedRect(
-    QRect(QPoint(0, 0), QSize(CLOCK_FACE_SIZE + OFFSET, CLOCK_FACE_SIZE + OFFSET)),
+    QRect(QPoint(0, 0), QSize(CLOCK_FACE_SIZE + OFFSET * 2, CLOCK_FACE_SIZE + OFFSET * 2)),
     qreal(ROUNDING),
     qreal(ROUNDING));
 #pragma endregion
@@ -101,8 +134,7 @@ Widget::paintEvent(QPaintEvent* event)
 
   painter.setBrush(m_colorClockFace);
   painter.setPen(Qt::NoPen);
-  painter.drawEllipse(
-    QRect(QPoint(OFFSET / 2, OFFSET / 2), QSize(CLOCK_FACE_SIZE, CLOCK_FACE_SIZE)));
+  painter.drawEllipse(QRect(QPoint(OFFSET, OFFSET), QSize(CLOCK_FACE_SIZE, CLOCK_FACE_SIZE)));
 #pragma region "Риски"
   pen.setColor(m_colorLabels);
   pen.setStyle(Qt::SolidLine);
@@ -132,12 +164,15 @@ Widget::paintEvent(QPaintEvent* event)
   painter.setRenderHint(QPainter::Antialiasing);
   painter.setRenderHint(QPainter::TextAntialiasing);
   painter.translate(m_center);
-  painter.rotate(currentTimeHour * 30 + currentTimeMinute / 2);
-  pen.setColor(m_colorHourArrow);
-  pen.setStyle(Qt::SolidLine);
-  painter.setPen(pen);
-  // painter.setBrush(m_colorMainArrows);
-  painter.drawPolygon(m_polygonArrowHour);
+  painter.rotate(currentTimeHour * 30 + currentTimeMinute / 2.f);
+
+  Arrow(painter, ARROW_HOUR_CENTER_CIRCLE, ARROW_HOUR_SIZE, ARROW_HOUR_TAIL_SIZE, m_colorHourArrow);
+  // pen.setColor(m_colorHourArrow);
+  // pen.setStyle(Qt::SolidLine);
+  // painter.setPen(pen);
+  // // painter.setBrush(m_colorMainArrows);
+  // painter.drawPolygon(m_polygonArrowHour);
+  // // painter.drawImage(QPoint(-m_center.x(), -m_center.y()), m_imgArrowHour);
   painter.restore();
 #pragma endregion
 #pragma region "Минутная стрелка"
@@ -145,12 +180,17 @@ Widget::paintEvent(QPaintEvent* event)
   painter.setRenderHint(QPainter::Antialiasing);
   painter.setRenderHint(QPainter::TextAntialiasing);
   painter.translate(m_center);
-  painter.rotate(currentTimeMinute * 6 + currentTimeSecond / 10);
-  pen.setColor(m_colorMinuteArrow);
-  pen.setStyle(Qt::SolidLine);
-  painter.setPen(pen);
-  // painter.setBrush(m_colorMainArrows);
-  painter.drawPolygon(m_polygonArrowMinute);
+  painter.rotate(currentTimeMinute * 6.f + currentTimeSecond / 10.f);
+  Arrow(painter,
+        ARROW_MINUTE_CENTER_CIRCLE,
+        ARROW_MINUTE_SIZE,
+        ARROW_MINUTE_TAIL_SIZE,
+        m_colorMinuteArrow);
+  // pen.setColor(m_colorMinuteArrow);
+  // pen.setStyle(Qt::SolidLine);
+  // painter.setPen(pen);
+  // // painter.setBrush(m_colorMainArrows);
+  // painter.drawPolygon(m_polygonArrowMinute);
   painter.restore();
 #pragma endregion
 #pragma region "Секундная стрелка"
@@ -158,12 +198,17 @@ Widget::paintEvent(QPaintEvent* event)
   painter.setRenderHint(QPainter::Antialiasing);
   painter.setRenderHint(QPainter::TextAntialiasing);
   painter.translate(m_center);
-  painter.rotate(currentTimeSecond * 6 + currentTimeMSec * 6 / 1000);
-  pen.setColor(m_colorSecondArrow);
-  pen.setStyle(Qt::SolidLine);
-  painter.setPen(pen);
-  // painter.drawPolyline(m_polygonArrowSecond);
-  painter.drawLine(m_lineArrowSecond);
+  painter.rotate(currentTimeSecond * 6.f + currentTimeMSec * 6.f / 1000);
+  Arrow(painter,
+        ARROW_SECOND_CENTER_CIRCLE,
+        ARROW_SECOND_SIZE,
+        ARROW_SECOND_TAIL_SIZE,
+        m_colorSecondArrow);
+  // pen.setColor(m_colorSecondArrow);
+  // pen.setStyle(Qt::SolidLine);
+  // painter.setPen(pen);
+  // // painter.drawPolyline(m_polygonArrowSecond);
+  // painter.drawLine(m_lineArrowSecond);
   painter.restore();
 #pragma endregion
 
